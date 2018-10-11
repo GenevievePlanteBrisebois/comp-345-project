0dbe1606b56f59aad9f55f9de5196b4aa77e6ad2
Batoul Yehia <batoul.yehia@gmail.com>
2018-10-11 14:19:19 -0400

    added the driver for maps and arranged folders

---
+++ Map Implementation/MapDriver.cpp
@@ -0,0 +1,56 @@
+#pragma once
+#include "Map.h"
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+
+using namespace std;
+
+const int N = 7;
+
+vector<string> adjacencyList[N];
+
+int main() {
+	string buroughList[7] = { "Staten Island", "Lower Manhattan", "Bronx", "Queens", "Brooklyn", "Mid Manhattan", "Upper Manhattan" };
+	//creation of a map object with 7 nodes, each representing a region
+	Map m(7);
+
+	//Adding edges to Staten Island.
+	m.addEdge(adjacencyList, 0, buroughList[1]);
+
+	//Adding Edges to Lower Manhattan
+	m.addEdge(adjacencyList, 1, buroughList[0]);
+	m.addEdge(adjacencyList, 1, buroughList[2]);
+	m.addEdge(adjacencyList, 1, buroughList[3]);
+	m.addEdge(adjacencyList, 1, buroughList[4]);
+	m.addEdge(adjacencyList, 1, buroughList[5]);
+
+	//Adding Edges to Bronx
+	m.addEdge(adjacencyList, 2, buroughList[1]);
+
+	//Adding Edges to Queens
+	m.addEdge(adjacencyList, 3, buroughList[1]);
+	m.addEdge(adjacencyList, 3, buroughList[4]);
+
+	//Adding Edges to Brooklyn
+	m.addEdge(adjacencyList, 4, buroughList[1]);
+	m.addEdge(adjacencyList, 4, buroughList[3]);
+
+	//Adding Edges to Mid Manhattan
+	m.addEdge(adjacencyList, 5, buroughList[0]);
+	m.addEdge(adjacencyList, 5, buroughList[2]);
+	m.addEdge(adjacencyList, 5, buroughList[3]);
+	m.addEdge(adjacencyList, 5, buroughList[4]);
+	m.addEdge(adjacencyList, 5, buroughList[6]);
+
+	//Adding Edges to Upper Manhattan
+	m.addEdge(adjacencyList, 6, buroughList[0]);
+	m.addEdge(adjacencyList, 6, buroughList[2]);
+	m.addEdge(adjacencyList, 6, buroughList[3]);
+	m.addEdge(adjacencyList, 6, buroughList[4]);
+
+	m.printMap(adjacencyList, N, buroughList);
+
+	return 0;
+}
