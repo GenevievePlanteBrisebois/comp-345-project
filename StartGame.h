74c3f7261394a5804c2e7f31030d2e981c9e7fee
Batoul Yehia <batoul.yehia@gmail.com>
2018-11-04 13:59:58 -0500

    what i did for a2 so far

---
+++ StartGame.h
@@ -0,0 +1,14 @@
+#pragma once
+class StartGame{
+public:
+	void SelectPlayers();
+	//StartGame();
+	~StartGame();
+	StartGame(player* player1, player* player2);
+	StartGame(player* player1, player* player2, player* player3);
+	StartGame(player* player1, player* player2, player* player3, player* player4);
+	StartGame(player* player1, player* player2, player* player3, player* player4, player* player5);
+	StartGame(player* player1, player* player2, player* player3, player* player4, player* player5, player* player6);
+	void TwoPlayers();
+	void LoadMap();
+};
