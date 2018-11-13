46b6592ae4b53e7fc0c66863a2213a0b615f23d9
Batoul Yehia <batoul.yehia@gmail.com>
2018-11-13 17:53:52 -0500

    fixed stuff in startgame.cpp

--- StartGame.cpp
+++ StartGame.cpp
@@ -3,12 +3,13 @@
 //Written by Batoul Yehia 40010912
 
 #include <iostream>
-#include "C:\Users\leb_b\Github\comp345-kingsOfNY\Cards\Cards_Deck.h"
-#include "C:\Users\leb_b\Github\comp345-kingsOfNY\Cards\Active_Monsters.h"
-#include "C:\Users\leb_b\Github\comp345-kingsOfNY\Cards\Tokens.h"
-#include "C:\Users\leb_b\Github\comp345-kingsOfNY\Dice\Dice.h"
-#include "C:\Users\leb_b\Github\comp345-kingsOfNY\Map Implementation\Map.h"
-#include "C:\Users\leb_b\Github\comp345-kingsOfNY\Player\Player\player.h"
+#include "..\comp345-kingsOfNY\Cards\Cards_Deck.h"
+#include "..\comp345-kingsOfNY\Cards\Active_Monsters.h"
+#include "..\comp345-kingsOfNY\Cards\Tokens.h"
+#include "..\comp345-kingsOfNY\Dice\Dice.h"
+#include "..\comp345-kingsOfNY\Map Implementation\Map.h"
+#include "..\comp345-kingsOfNY\Player\Player\player.h"
+
 #include <string>
 #include "StartGame.h"
 #include <fstream>
@@ -32,7 +33,6 @@ player* players5[5];
 player* players6[6];
 //player* playerArray[numPlayers];
 
-Cards_Deck* deck;
 
 //constructor for two players
 StartGame::StartGame(player* player1, player* player2) {
@@ -109,28 +109,28 @@ void StartGame::SelectPlayers() {
 	switch (num) {
 	case 2:
 		StartGame(p1, p2);
-		SetNumPlayers(2);
+		
 		break;
 	case 3:
 		//creates three players
 		StartGame(p1, p2, p3);
-		SetNumPlayers(3);
+	
 		break;
 
 	case 4:
 		//creates four players
 		StartGame(p1, p2, p3, p4);
-		SetNumPlayers(4);
+	
 		break;
 	case 5:
 		//creates five players
 		StartGame(p1, p2, p3, p4, p5);
-		SetNumPlayers(5);
+
 		break;
 	case 6:
 		//creates six players
 		StartGame(p1, p2, p3, p4, p5, p6);
-		SetNumPlayers(6);
+		
 		break;
 	}
 	SetNumPlayers(num);
@@ -147,7 +147,7 @@ int StartGame::GetNumPlayers() {
 void StartGame::LoadMap() {
 	string chosenMap;
 	string line;
-	int count;
+	int count=0;
 
 	cout << "Please select one of the following map files: " << endl;
 	//idk somehow shows directory
@@ -174,11 +174,11 @@ StartGame::~StartGame() {
 	d = NULL;
 
 	
-	delete[] players2;
-	delete[] players3;
-	delete[] players4;
-	delete[] players5;
-	delete[] players6;
+	delete[] * players2;
+	delete[] * players3;
+	delete[] * players4;
+	delete[] * players5;
+	delete[] * players6;
 	delete p1;
 	delete p2;
 	delete p3;
@@ -203,7 +203,7 @@ void StartGame::BuildTokens() {
 	
 	Tokens* souvenir[5];
 	for (int i = 0; i < 5; i++) {
-		souvenir[i] - token->create_souvenir();
+		souvenir[i] = token->create_souvenir();
 	}
 
 	Tokens* web[13];
@@ -235,7 +235,7 @@ int SelectFirst() {
 		cout << "Player " << (i + 1) << "dice: ";
 		for (int j = 0; j < 8; j++) { //roll dice 8 times
 			d->rollDice(rand() % 5);
-			face = d->getDiceFace;
+			face = d->getDiceFace();
 			if (face == "Attack") {
 				count++;
 			}
@@ -258,155 +258,155 @@ void SelectOrder(int max) {
 
 	switch (numPlayers) {
 	case 2:
-		player* order[2];
+		player* orderTwo[2];
 		if (max == 1) { //if player 1 got the the most attacks, they start first
-			order[0] = p1;
-			order[1] = p2;
+			orderTwo[0] = p1;
+			orderTwo[1] = p2;
 		}
 		if (max == 2) { //if player 2 got the most attacks, they start first
-			order[0] = p2;
-			order[1] = p1;
+			orderTwo[0] = p2;
+			orderTwo[1] = p1;
 		}
-		setPlayerOrder2(order); //set the order of the players based on what is given
+		setPlayerOrder2(orderTwo); //set the order of the players based on what is given
 		break;
 	case 3:
-		player* order[3];
+		player* orderThree[3];
 		if (max == 1) {
-			order[0] = p1;
-			order[1] = p2;
-			order[2] = p3;
+			orderThree[0] = p1;
+			orderThree[1] = p2;
+			orderThree[2] = p3;
 		}
 		if (max == 2) {
-			order[0] = p2;
-			order[1] = p1;
-			order[2] = p3;
+			orderThree[0] = p2;
+			orderThree[1] = p1;
+			orderThree[2] = p3;
 		}
 		if (max == 3) {
-			order[0] = p3;
-			order[1] = p1;
-			order[2] = p2;
+			orderThree[0] = p3;
+			orderThree[1] = p1;
+			orderThree[2] = p2;
 		}
-		setPlayerOrder3(order);
+		setPlayerOrder3(orderThree);
 		break;
 	case 4:
-		player* order[4];
+		player* orderFour[4];
 		if (max == 1) {
-			order[0] = p1;
-			order[1] = p2;
-			order[2] = p3;
-			order[3] = p4;
+			orderFour[0] = p1;
+			orderFour[1] = p2;
+			orderFour[2] = p3;
+			orderFour[3] = p4;
 		}
 		if (max == 2) {
-			order[0] = p2;
-			order[1] = p1;
-			order[2] = p3;
-			order[3] = p4;
+			orderFour[0] = p2;
+			orderFour[1] = p1;
+			orderFour[2] = p3;
+			orderFour[3] = p4;
 		}
 		if (max == 3) {
-			order[0] = p3;
-			order[1] = p1;
-			order[2] = p2;
-			order[3] = p4;
+			orderFour[0] = p3;
+			orderFour[1] = p1;
+			orderFour[2] = p2;
+			orderFour[3] = p4;
 		}
 		if (max == 4) {
-			order[0] = p4;
-			order[1] = p1;
-			order[2] = p2;
-			order[3] = p3;
+			orderFour[0] = p4;
+			orderFour[1] = p1;
+			orderFour[2] = p2;
+			orderFour[3] = p3;
 		}
-		setPlayerOrder4(order);
+		setPlayerOrder4(orderFour);
 		break;
 	case 5:
-		player* order[5];
+		player* orderFive[5];
 		if (max == 1) {
-			order[0] = p1;
-			order[1] = p2;
-			order[2] = p3;
-			order[3] = p4;
-			order[4] = p5;
+			orderFive[0] = p1;
+			orderFive[1] = p2;
+			orderFive[2] = p3;
+			orderFive[3] = p4;
+			orderFive[4] = p5;
 		}
 		if (max == 2) {
-			order[0] = p2;
-			order[1] = p1;
-			order[2] = p3;
-			order[3] = p4;
-			order[4] = p5;
+			orderFive[0] = p2;
+			orderFive[1] = p1;
+			orderFive[2] = p3;
+			orderFive[3] = p4;
+			orderFive[4] = p5;
 		}
 		if (max == 3) {
-			order[0] = p3;
-			order[1] = p1;
-			order[2] = p2;
-			order[3] = p4;
-			order[4] = p5;
+			orderFive[0] = p3;
+			orderFive[1] = p1;
+			orderFive[2] = p2;
+			orderFive[3] = p4;
+			orderFive[4] = p5;
 		}
 		if (max == 4) {
-			order[0] = p4;
-			order[1] = p1;
-			order[2] = p2;
-			order[3] = p3;
-			order[4] = p5;
+			orderFive[0] = p4;
+			orderFive[1] = p1;
+			orderFive[2] = p2;
+			orderFive[3] = p3;
+			orderFive[4] = p5;
 		}
 		if (max == 5) {
-			order[0] = p5;
-			order[1] = p1;
-			order[2] = p2;
-			order[3] = p3;
-			order[4] = p4;
+			orderFive[0] = p5;
+			orderFive[1] = p1;
+			orderFive[2] = p2;
+			orderFive[3] = p3;
+			orderFive[4] = p4;
 		}
-		setPlayerOrder5(order);
+		setPlayerOrder5(orderFive);
 		break;
 	case 6:
-		player* order[6];
+		player* orderSix[6];
 		if (max == 1) {
-			order[0] = p1;
-			order[1] = p2;
-			order[2] = p3;
-			order[3] = p4;
-			order[4] = p5;
-			order[5] = p6;
+			orderSix[0] = p1;
+			orderSix[1] = p2;
+			orderSix[2] = p3;
+			orderSix[3] = p4;
+			orderSix[4] = p5;
+			orderSix[5] = p6;
 
 		}
 		if (max == 2) {
-			order[0] = p2;
-			order[1] = p1;
-			order[2] = p3;
-			order[3] = p4;
-			order[4] = p5;
-			order[5] = p6;
+			orderSix[0] = p2;
+			orderSix[1] = p1;
+			orderSix[2] = p3;
+			orderSix[3] = p4;
+			orderSix[4] = p5;
+			orderSix[5] = p6;
 		}
 		if (max == 3) {
-			order[0] = p3;
-			order[1] = p1;
-			order[2] = p2;
-			order[3] = p4;
-			order[4] = p5;
-			order[5] = p6;
+			orderSix[0] = p3;
+			orderSix[1] = p1;
+			orderSix[2] = p2;
+			orderSix[3] = p4;
+			orderSix[4] = p5;
+			orderSix[5] = p6;
 		}
 		if (max == 4) {
-			order[0] = p4;
-			order[1] = p1;
-			order[2] = p2;
-			order[3] = p3;
-			order[4] = p5;
-			order[5] = p6;
+			orderSix[0] = p4;
+			orderSix[1] = p1;
+			orderSix[2] = p2;
+			orderSix[3] = p3;
+			orderSix[4] = p5;
+			orderSix[5] = p6;
 		}
 		if (max == 5) {
-			order[0] = p5;
-			order[1] = p1;
-			order[2] = p2;
-			order[3] = p3;
-			order[4] = p4;
-			order[5] = p6;
+			orderSix[0] = p5;
+			orderSix[1] = p1;
+			orderSix[2] = p2;
+			orderSix[3] = p3;
+			orderSix[4] = p4;
+			orderSix[5] = p6;
 		}
 		if (max == 6) {
-			order[0] = p6;
-			order[1] = p1;
-			order[2] = p2;
-			order[3] = p3;
-			order[4] = p4;
-			order[5] = p5;
+			orderSix[0] = p6;
+			orderSix[1] = p1;
+			orderSix[2] = p2;
+			orderSix[3] = p3;
+			orderSix[4] = p4;
+			orderSix[5] = p5;
 		}
-		setPlayerOrder6(order);
+		setPlayerOrder6(orderSix);
 		break;
 
 	}
