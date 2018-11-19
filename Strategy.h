120671cf038be2d7115115e99f75fce620ea57eb
Batoul Yehia <batoul.yehia@gmail.com>
2018-11-18 19:09:50 -0500

    working on the bots

--- Strategy.h
+++ Strategy.h
@@ -4,6 +4,23 @@
 using namespace std;
 class StrategyInterface {
 public:
-	//virtual void idkSomeAction();
-	//lmao do something
+	virtual void rollDice();
+	virtual void resolveDice();
+	virtual void move();
+	virtual void buyCards();
 };
+class Aggressive : public StrategyInterface {
+public:
+	void rollDice() override;
+	void resolveDice() override;
+	void move() override;
+	void buyCards() override;
+};
+
+class Moderate : public StrategyInterface {
+	void rollDice() override;
+	void resolveDice() override;
+	void move() override;
+	void buyCards() override;
+};
+
